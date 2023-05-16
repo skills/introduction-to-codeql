c11958a99bdfd4b32398002d5798072c12ce7536
Dan Shanahan <leftrightleft@github.com>
2023-05-16 12:46:52 -0700

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -13,7 +13,7 @@ def index():
 
     if name:
         cursor.execute(
-            "SELECT * FROM books WHERE name LIKE %s", name
+            "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
         )
         books = [Book(*row) for row in cursor]
 
